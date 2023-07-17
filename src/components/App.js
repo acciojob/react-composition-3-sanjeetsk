86f0aa457687481f5acb92a6d94b1a715656abd4
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-07-17 13:07:29 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Tooltip text="This is a tooltip">
+          <h1>Hover over me</h1>
+        </Tooltip>
+
+        <Tooltip text="This is another tooltip">
+          <p>Hover over me to see another tooltip</p>
+        </Tooltip>
+              
     </div>
   )
 }
+
+const Tooltip = ({ text, children }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
+  return (
+    <div className="tooltip" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {children}
+      {
+        showTooltip && <button className="tooltiptext">{text}</button>
+      }
+    </div>
+  );
+};
 
 export default App
